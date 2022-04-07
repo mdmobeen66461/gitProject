4799b76ed949e17098f4f574b01b8d1064e8c84b
mobeen <m.mobeen@zensar.com>
2022-04-07 17:35:20 +0530

    added two files

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		int number=10;
+		
+		if(number%2==0) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
