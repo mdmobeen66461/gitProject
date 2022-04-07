7d20f4059fcf9bbab5b88901cfe95e79a16c69d1
mobeen <m.mobeen@zensar.com>
2022-04-07 17:47:48 +0530

    modified OddEven.java file

--- OddEven.java
+++ OddEven.java
@@ -1,9 +1,10 @@
-
-
+import java.util.Scanner;
 public class OddEven {
 
 	public static void main(String[] args) {
-		int number=10;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int numbersc.nextInt();
 		
 		if(number%2==0) {
 			System.out.println("Even");
